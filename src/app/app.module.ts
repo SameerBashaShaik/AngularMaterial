c83d8751fe696f7aabfbeca73f47b7cc68d2534d
Sameer Basha Shaik <sameerbasha.shaik@hach.com>
2020-07-01 16:18:13 +0530

    Code Commit - Initial routing

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import {Routes, RouterModule} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import {DemoModule} from './demo/demo.module';
+
+
+const routes:Routes = [
+
+  { path: 'demo', loadChildren: './demo/demo.module#DemoModule' },
+  { path: '**', redirectTo: 'demo' }
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    DemoModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
